61efcd509868b4b419d6a90a9c9b1437f5d3f8d4
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-02-23 15:05:01 -0500

    Card object deck is properly made. splitCardDeck function randomly splits the card deck between two players

--- index.ts
+++ index.ts
@@ -6,47 +6,63 @@ program
   .description('Creating wargame in CLI')
   .version('0.8.0');
 
-// function shuffleCards() {
-//     const cards = [];
-//     for (let i = 0; i < 52; i++) {
-//         cards.push(i);
-//     }
-//     for (let i = 0; i < 52; i++) {
-//         const j = Math.floor(Math.random() * 52);
-//         const temp = cards[i];
-//         cards[i] = cards[j];
-//         cards[j] = temp;
-//     }
-//     return cards;
-// }
-
 interface Card {
     suit: string;
     value: number;
     name: string;
 }
 
-const cards: object[] = [];
-const suits: string[] = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-for (const suit of suits) {
-    for (let i = 2; i <= 14; i++) {
-        let name = i.toString();
-        if (i === 11) {
-            name = 'Jack';
-        } else if (i === 12) {
-            name = 'Queen';
-        } else if (i === 13) {
-            name = 'King';
-        } else if (i === 14) {
-            name = 'Ace';
+let playerOneHand: Card[] = [];
+let playerTwoHand: Card[] = [];
+
+function buildDeck() {
+    const cards: Card[] = [];
+    const suits: string[] = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    for (const suit of suits) {
+        for (let i = 2; i <= 14; i++) {
+            let name = i.toString();
+            if (i === 11) {
+                name = 'Jack';
+            } else if (i === 12) {
+                name = 'Queen';
+            } else if (i === 13) {
+                name = 'King';
+            } else if (i === 14) {
+                name = 'Ace';
+            }
+            cards.push({
+                suit,
+                value: (i === 11 || i === 12 || i === 13) ? 10 : i,
+                name,
+            });
         }
-        cards.push({
-            suit,
-            value: i === (11 || 12 || 13) ? 10 : i,
-            name,
-        });
+    }
+    return cards;
+}
+let standardCardDeck: Card[] = buildDeck();
+function splitCardDeck() {
+    let deckSize = 52
+    for (let i = 0; i < 52; i++) {
+        let randomSelection: number = Math.floor(Math.random() * deckSize);
+        if (i % 2 === 0) {
+            playerOneHand.push(standardCardDeck[randomSelection]);
+            standardCardDeck = standardCardDeck.filter(function( obj ) {
+                return !(obj.name === standardCardDeck[randomSelection].name && obj.suit === standardCardDeck[randomSelection].suit);
+              });
+        } else {
+            playerTwoHand.push(standardCardDeck[randomSelection]);
+            standardCardDeck = standardCardDeck.filter(function( obj ) {
+                return !(obj.name === standardCardDeck[randomSelection].name && obj.suit === standardCardDeck[randomSelection].suit);
+              });
+        }
+        deckSize--;
     }
 }
+splitCardDeck();
+console.log("PlayerOneHand");
+console.log(playerOneHand);
+console.log("PlayerTwoHand")
+console.log(playerTwoHand);
 
 function shuffleCards() {
     let counter = 0;
@@ -67,16 +83,6 @@ program.command('start-game')
         console.log('Game started');
         shuffleCards();
     });
-
-program.command('split')
-.description('Split a string into substrings and display as an array')
-.argument('<string>', 'string to split')
-.option('--first', 'display just the first substring')
-.option('-s, --separator <char>', 'separator character', ',')
-.action((str, options) => {
-const limit = options.first ? 1 : undefined;
-console.log(str.split(options.separator, limit));
-});
 
 program.parse();
 
